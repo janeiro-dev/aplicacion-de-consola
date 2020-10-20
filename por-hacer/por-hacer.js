112e929a79c917e081de1377dd687d6b6dfb44c5
Janeiro <psiqui99@gmail.com>
2020-10-20 08:10:17 -0400

    Aplicacion de consola

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,111 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+
+
+
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile(`db/data.json`, data, (err) => {
+        if (err) throw new Error(err);
+
+    });
+
+
+}
+
+const cargarDB = () => {
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch {
+        listadoPorHacer = [];
+    }
+
+
+}
+
+const crear = (descripcion) => {
+    cargarDB();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+    guardarDB();
+
+    return porHacer;
+}
+
+const getListado = () => {
+    cargarDB();
+    return listadoPorHacer;
+}
+
+const actualizar = (descripcion, completado = true) => {
+
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+const borrar = (descripcion) => {
+    cargarDB();
+
+    let nuevoListado = listadoPorHacer.filter(tarea => {
+        return tarea.descripcion !== descripcion
+    });
+
+    if (listadoPorHacer.length === nuevoListado.length) {
+        return false;
+    } else {
+        listadoPorHacer = nuevoListado;
+        guardarDB();
+        return true;
+    }
+
+
+}
+
+const estatus = () => {
+
+
+    var completado = true;
+    var incompletado = false;
+
+    for (var completados of listadoPorHacer) {
+        cargarDB();
+        if (listadoPorHacer.completado === completado) {
+            completados += listadoPorHacer.descripcion;
+        }
+    }
+
+    for (var incompletados of listadoPorHacer) {
+        if (listadoPorHacer.completado == incompletado) {
+            incompletados += listadoPorHacer.descripcion;
+        }
+    }
+
+    return completados;
+
+    // return `Completados: \n ${completados} \n Incompletados: \n ${incompletados}`;
+    // return completados;
+}
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar,
+    estatus
+}
